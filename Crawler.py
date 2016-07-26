5a7655f45a79f63a59ffbcd5d8ce9ea36ce2786b
Benjamin <frost1995@live.dk>
2016-07-26 20:35:24 +0200

    Revert "Revert "Added crawler.py""
    This reverts commit 068819667385d5a415d266b153101b20c4955cc4.

---
+++ Crawler.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+
+links = []
+visited = []
+allLinks = []
+
+
+def crawler(url, maxPages):
+    links.append(url)
+    url = links.pop()
+    page = 0
+    while page < maxPages:
+        try:
+            visited.append(url)
+            code = requests.get(url)
+            plain_text = code.text
+            soup = BeautifulSoup(plain_text, "html.parser")
+            for link in soup.findAll("a"):
+                href = link.get("href")
+                if href.split()[0][0] == "/":
+                    href = url + href
+                if href.split()[0][0] == "#":
+                    href = url + "/" + href
+                if href not in allLinks:
+                    links.append(href)
+                    allLinks.append(href)
+        except:
+            if url == None:
+                print(str(url)+" findes ikke")
+
+        if len(links) == 0:
+            print("har ikke flere links")
+            break
+        url = links.pop()
+        page += 1
+
+    print("links found: " + str(allLinks))
+    print("visited: " + str(visited))
+
+crawler("http://www.doggycraft.dk", 5)  # url and maxPages to crawl
